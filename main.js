bcc30241fe6e717887a99bd766ffb52b7dd9fa64
Abdullah-Alsuedani <abdullah.xvx.2006@gmail.com>
2022-12-18 17:27:52 +0300

    update

--- main.js
+++ main.js
@@ -4,9 +4,20 @@ let bar2 = document.querySelector("header .bar span:nth-child(2)");
 let bar3 = document.querySelector("header .bar span:last-child");
 let menu = document.querySelector("header .nav");
 
+let lies = document.querySelectorAll(".nav li")
+
 bar.onclick = function () {
   menu.classList.toggle("active");
   bar1.classList.toggle("first");
   bar2.classList.toggle("mid");
   bar3.classList.toggle("last");
 };
+
+lies.forEach((el) => {
+    el.addEventListener("click", function(){
+        menu.classList.toggle("active");
+        bar1.classList.toggle("first");
+        bar2.classList.toggle("mid");
+        bar3.classList.toggle("last");
+    })
+})
